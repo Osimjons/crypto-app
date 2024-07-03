b4c7dd2559d25857509634357468e43436efcf03
osimjon <osimjon90@yandex.ru>
2024-07-03 17:08:34 +0300

    Connect API key

--- src/API/fakeFetch.js
+++ src/API/fakeFetch.js
@@ -1,12 +1,28 @@
-import { CRYPTO_ASSETS, CRYPTO_DATA } from '../Data/DATA';
+import { CRYPTO_ASSETS } from '../Data/DATA';
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const fakeFetchCriptoData = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(CRYPTO_DATA);
-    }, 150);
-  });
+export const fakeFetchCriptoData = async () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      'X-API-KEY': API_KEY,
+    },
+  };
+
+  try {
+    const resp = await fetch(BASE_URL, options);
+    const data = await resp.json();
+    console.log('data: ', data);
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching crypto data:', error);
+    return null;
+  }
 };
+
 export const fakeFetchCriptoAssets = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
